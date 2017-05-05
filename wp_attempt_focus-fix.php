ef5ad91131cc5bd8d81cd741b29159fb22ef210d
Eric Nemchik <eric@nemchik.com>
2017-05-05 10:11:31 -0500

    Initial Commit

---
+++ wp_attempt_focus-fix.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: wp_attempt_focus Fix
+Description: https://wordpress.stackexchange.com/a/163525/92974
+Version: 1.0
+Author: Robbert
+*/
+add_action("login_form", "kill_wp_attempt_focus_start");
+function kill_wp_attempt_focus_start() {
+    ob_start("kill_wp_attempt_focus_replace");
+}
+
+function kill_wp_attempt_focus_replace($html) {
+    return preg_replace("/d.value = '';/", "", $html);
+}
+
+add_action("login_footer", "kill_wp_attempt_focus_end");
+function kill_wp_attempt_focus_end() {
+    ob_end_flush();
+}
